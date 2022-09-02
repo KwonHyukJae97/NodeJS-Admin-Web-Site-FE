99e2bf5c346b7ffabc1174e1168d3366d8c6bf03
klai-tech01 <tech01@doubledlab.co.kr>
2022-09-02 15:52:38 +0900

    최초 풀 소스 기준으로 커밋 로컬 백앤드 로그인 관련 로직 추가 연동

---
+++ src/configs/auth.ts
@@ -0,0 +1,12 @@
+export default {
+  meEndpoint: 'http://localhost:3000/auth/me',
+  loginEndpoint: 'http://localhost:3000/auth/login/admin',
+  registerEndpoint: '/jwt/register',
+  storageTokenKeyName: 'authorization'
+}
+// export default {
+//   meEndpoint: '/auth/me',
+//   loginEndpoint: '/jwt/login',
+//   registerEndpoint: '/jwt/register',
+//   storageTokenKeyName: 'authorization'
+// }
